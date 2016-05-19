db121da53274df5f912cb364db632e45e5d185d6
victoralonsopacha <victoralonsopacha@outlook.es>
2016-05-19 08:54:47 -0500

    promedio version con funcion
    aplicandolo en una funcion

--- promedio.py
+++ promedio.py
@@ -1,13 +1,17 @@
-suma=1
-contador=0
-result=0
-while suma != 0:
-    suma=int(input('ingrese un numero a sumar:'))
-    result=result+suma
-    contador=contador+1
-    promedio=result/contador
-    print ('el promedio es:')
-    print(promedio)
+
+def promedio():
+    suma=1
+    contador=0
+    result=0
+    while suma != 0:
+        suma=int(input('ingrese un numero a sumar:'))
+        result=result+suma
+        contador=contador+1
+        promedio=result/contador
+        print ('el promedio es:')
+        print(promedio)
+    
+promedio()
     
 
     
